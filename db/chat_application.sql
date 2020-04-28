b5934af89f2389facef71813669ddbea50ec54c7
kimarinov <58824713+kimarinov@users.noreply.github.com>
2020-04-28 23:13:24 +0300

    sun db

--- db/chat_application.sql
+++ db/chat_application.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 12, 2020 at 07:59 PM
+-- Generation Time: Apr 28, 2020 at 10:12 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -31,24 +31,39 @@ SET time_zone = "+00:00";
 CREATE TABLE `message` (
   `id` int(11) NOT NULL,
   `message` text NOT NULL,
-  `user_name` varchar(100) NOT NULL
+  `user_name` varchar(100) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `message`
 --
 
-INSERT INTO `message` (`id`, `message`, `user_name`) VALUES
-(1, 'how are you', 'computermaster'),
-(2, 'how are you', 'computermaster'),
-(3, 'fine', 'computermaster'),
-(4, 'xaxa', 'computermaster'),
-(5, 'xaxa', 'computermaster'),
-(6, 'xaxa', 'computermaster'),
-(7, 'xaxa', 'computermaster'),
-(8, 'xaxa', 'computermaster'),
-(9, 'xaxa', 'computermaster'),
-(10, 'how old are you ? ', 'computermasterOne');
+INSERT INTO `message` (`id`, `message`, `user_name`, `date_created`) VALUES
+(180, '1', '123', '2020-04-28 00:00:00'),
+(181, ';l', '123', '2020-04-28 00:00:00'),
+(182, 'fefe', '123', '2020-04-28 00:00:00'),
+(183, 'dwdw', '123', '2020-04-28 00:00:00'),
+(184, 'ew', '123', '2020-04-28 00:00:00'),
+(185, '20320', '123', '2020-04-28 00:00:00'),
+(186, '2', '123', '2020-04-28 00:00:00'),
+(187, '5', '123', '2020-04-28 00:00:00'),
+(188, '1051', '123', '2020-04-28 00:00:00'),
+(189, '61065156', '123', '2020-04-28 00:00:00'),
+(190, '51515', '123', '2020-04-28 00:00:00'),
+(191, '65165165', '123', '2020-04-28 00:00:00'),
+(192, '651561651', '123', '2020-04-28 00:00:00'),
+(193, '6516516516', '123', '2020-04-28 00:00:00'),
+(194, '65165', '123', '2020-04-28 00:00:00'),
+(195, '656+2', '123', '2020-04-28 00:00:00'),
+(196, 'e', '2', '2020-04-28 00:00:00'),
+(197, 'rewrw', '2', '2020-04-28 00:00:00'),
+(198, 'des', '2', '2020-04-28 00:00:00'),
+(199, 'erww', '2', '2020-04-28 00:00:00'),
+(200, 'ererer', '2', '2020-04-28 22:26:54'),
+(201, 'rewrwe', 'koko', '2020-04-28 22:38:50'),
+(202, 'fsesefsefs', '123', '2020-04-28 22:54:46'),
+(203, 'sdadas', '123', '2020-04-28 22:54:55');
 
 -- --------------------------------------------------------
 
@@ -73,13 +88,10 @@ INSERT INTO `user` (`id`, `first_name`, `last_name`, `user_name`, `password`, `u
 (1, 'computer', 'master', 'computermaster', 'computer', 1),
 (2, 'computer', 'master', 'computermaster', 'computer', 1),
 (3, 'computerOne', 'masterOne', 'computermasterOne', 'computerOne', 1),
-(4, 'asdasda', '', 'fewdasdasda', '611fc48cf4e68a30efb69e669869b08b', 0),
-(5, 'weqeq', '', 'asdasda', '06474975137c13d577c8d75ce1a776f1', 0),
-(6, '@', '', 'asad', '5f039b4ef0058a1d652f13d612375a5b', 0),
-(7, '2', '', '2', 'c81e728d9d4c2f636f067f89cc14862c', 0),
-(8, '2', '', '2', 'c81e728d9d4c2f636f067f89cc14862c', 0),
 (9, 'lee', '', 'lee', '7e0d7f8a5d96c24ffcc840f31bce72b2', 2),
-(10, 'admin', '', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1);
+(10, 'admin', '', 'admin', '21232f297a57a5a743894a0e4a801fc3', 1),
+(11, '123', '', '123', '202cb962ac59075b964b07152d234b70', 2),
+(12, 'koko', '', 'koko', '202cb962ac59075b964b07152d234b70', 2);
 
 -- --------------------------------------------------------
 
@@ -131,13 +143,13 @@ ALTER TABLE `user_types`
 -- AUTO_INCREMENT for table `message`
 --
 ALTER TABLE `message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=204;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `user_types`
